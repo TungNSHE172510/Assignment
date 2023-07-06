6557bf1b752952eadf01d70d4590e3201ae0ff9a
Tung <tungnshe172510@fpt.edu.vn>
2023-07-06 23:26:12 +0700

    fix context

--- Assignment/src/java/context/DBContext.java
+++ Assignment/src/java/context/DBContext.java
@@ -6,25 +6,22 @@ package context;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
- * @author kiemq
+ * @author AD
  */
+//DBContext
 public class DBContext {
-    protected Connection connection;
-    public DBContext(){
-        try{
-            String user = "sa";
-            String pass = "123123";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=testDb";
+	public Connection getConnection() throws Exception {	       
+            String url = "jdbc:sqlserver:// " + serverName+":" + portNumber+";databaseName="+dbName;
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(url, user, pass);
-        }catch(ClassNotFoundException|SQLException ex){
-            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            return DriverManager.getConnection(url, userID, password);
         }
-    }
+        private final String serverName = "localhost";
+        private final String dbName = "testDb";
+        private final String portNumber = "1433";
+        private final String userID = "sa";
+        private final String password = "12345678";
 }
+
